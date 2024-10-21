58cb85ead75a97de42e81f8bb4018cc5c886d3f1
Jihyuk Lee <lead@sver.dev>
2024-10-21 12:22:20 +0900

    chore :: 사용하지 않는 코드 제거

--- review/review_dto.py
+++ review/review_dto.py
@@ -3,21 +3,5 @@ from pydantic import BaseModel
 
 class CreateReviewReqeustDto(BaseModel):
     rating: int
-    userId: int
     content: str
     productId: int
-
-def create_review_dto(data: dict) -> CreateReviewReqeustDto:
-    return CreateReviewReqeustDto(
-        rating=data['rating'],
-        content=data['content'],
-        userId=data['userId'],
-        productId=data['productId']
-    )
-
-def get_review_dto(data: dict) -> CreateReviewReqeustDto:
-    return CreateReviewReqeustDto(
-        rating=data['rating'],
-        content=data['content'],
-        userId=data['userId']
-    )
