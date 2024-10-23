708a7de4451b0bf7e784ca840ef6c23dc03a1da1
Jihyuk Lee <lead@sver.dev>
2024-10-23 10:05:36 +0900

    fix :: 연관된 필드까지 null로 출력 되는 문제 해결

--- product/dto/product_dto.py
+++ product/dto/product_dto.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 from enum import Enum
 
 
@@ -6,11 +7,34 @@ class StatusEnum(str, Enum):
     ON_SALE = "ON_SALE"
     SOLD_OUT = "SOLD_OUT"
 
+class Option(BaseModel):
+    id: int
+    productId: int
+    name: str
+    shoulder: float
+    towPiece: float
+    chest: float
+    sleeve: float
 
 class ProductResponseDTO(BaseModel):
     id: int
     price: int
     rating: int
-    status: StatusEnum
+    status: Optional[StatusEnum]
     title: str
     content: str
+
+
+
+def create_product_list(data_list: List[dict]) -> List[ProductResponseDTO]:
+    return [
+        ProductResponseDTO(
+            id=item.id,
+            price=item.price,
+            rating=item.rating,
+            status=item.status,
+            title=item.title,
+            content=item.content,
+        )
+        for item in data_list
+    ]
