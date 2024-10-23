4d4640f27848e2fb6eb5a6af2f5d373367cc4226
Jihyuk Lee <lead@sver.dev>
2024-10-23 10:06:30 +0900

    fix :: 단일 상품 조회시, 연관 필드로 옵션 추가

--- product/product_service.py
+++ product/product_service.py
@@ -2,7 +2,9 @@ from common.db import db
 
 
 async def get_product(products_id: int):
-    return await db.products.find_unique(where={"id": products_id})
+    return await db.products.find_unique(where={"id": products_id}, include={
+            "option" : True
+    })
 
 
 async def get_all_product():
