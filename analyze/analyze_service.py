586fb95d86bafb6b5b5259f72e36612831307798
Jihyuk Lee <lead@sver.dev>
2024-11-01 11:50:07 +0900

    chore :: 프롬프트 롤백

--- analyze/analyze_service.py
+++ analyze/analyze_service.py
@@ -13,8 +13,7 @@ def analyze_image(base64_image):
                 "content": [
                     {
                         "type": "text",
-                        "text": "Based on the user's physical attributes and the clothing material, assess if the outfit in the image would fit and suit the user. The user is 174cm weighs 64kg, and has sensitive skin. Analyze how well the outfit in the image will fit the user's body, if the material will be suitable for their skin sensitivity, and if the clothing size is appropriate. Also, determine if the style complements the user's physique. Respond in Korean.",
-                        # Provide detailed descriptions of clothing in photos for visually impaired individuals in Korean.
+                        "text": "Provide detailed descriptions of clothing in photos for visually impaired individuals in Korean."
                     },
                     {
                         "type": "image_url",
